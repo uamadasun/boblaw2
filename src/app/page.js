444424b3159c2a895cd1e04827dfa918156225e6
uamadasun <uchennaamadasun@gmail.com>
2024-01-30 13:11:29 -0500

    testing if production site is updated.

--- src/app/page.js
+++ src/app/page.js
@@ -1,45 +1,58 @@
-"use client";
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React from "react";
 
-const Home = () => {
-  const [posts, setPosts] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const reqUrl = `${process.env.NEXT_PUBLIC_WORDPRESS_API_URL}/wp-json/wp/v2/posts`;
-
-  const fetchPosts = async () => {
-    setLoading(true);
-
-    try {
-      const res = await axios.get(reqUrl);
-      console.log("results from call", res.data); // Access the response data
-      setPosts(res.data); // Update the posts state with fetched data
-    } catch (error) {
-      console.log("this is the error message", error);
+async function getPosts() {
+  const query = `
+    {
+      posts {
+        edges {
+          node {
+            id
+            title
+            excerpt
+            slug
+          }
+        }
+      }
     }
+  `;
+  const res = await fetch(
+    `${process.env.WORDPRESS_API_URL}?query=${encodeURIComponent(query)}`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
 
-    setLoading(false);
-  };
-
-  useEffect(() => {
-    fetchPosts();
-    console.log("I am in useEffect");
-  }, []);
-  console.log("finally i have posts: ", posts)
-
-  return (
-    <div>
-      {loading ? (
-        "Loading..."
-      ) : (
-        posts.map((post) => (
-          <p key={post.id} className="text-red font-black text-lg">
-            {post.title.rendered}
-          </p>
-        ))
-      )}
-    </div>
+      next: {
+        revalidate: 0,
+      },
+    }
   );
-};
+  const { data } = await res.json();
+  console.log("result data: ", data);
+  return data;
+}
 
-export default Home;
+export default async function Home() {
+    const fetchedPosts = await getPosts();
+    
+    await console.log("fetched posts:", fetchedPosts);
+
+    return (
+      <div>
+        <div className="w-full h-20 mt-40 bg-blue-400">
+          <p>hello</p>
+          {fetchedPosts.posts.edges.map((post) => (
+            <div key={post.id} className="font-black text-red">
+              <h2 className="font-black text-red">{post.node.title}</h2>
+              {post.node.excerpt}
+              <p>Slug: {post.node.slug}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+
+  
+}
